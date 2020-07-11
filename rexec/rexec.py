0c38458d37ef93ba6e286361ad6bfd55b5e0fb13
danx0r <dan.miller@eye0.com>
2020-07-11 15:48:04 -0700

    get ready

--- rexec/rexec.py
+++ rexec/rexec.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 import os, sys, argparse
 
-print ("rexec")
+DEfAULT_IMAGE = "rexec_image"
+
+print ("rexec -- remote execution using docker containers")
+
+cmd = "docker build . -t {0}".format(DEfAULT_IMAGE)
+print (cmd)
+
+args = " ".join(sys.argv[1:])
+print ("ARGS:", args)
+cmd = "docker run --rm -it {0} {1}".format(DEfAULT_IMAGE, args)
+print (cmd)
