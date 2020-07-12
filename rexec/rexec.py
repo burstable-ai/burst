33d93a7ff86e4699e904e30740a20aa21108d368
danx0r <dan.miller@eye0.com>
2020-07-12 14:23:23 -0700

    main attraction

--- rexec/rexec.py
+++ rexec/rexec.py
@@ -5,12 +5,24 @@ DEfAULT_IMAGE = "rexec_image"
 
 print ("rexec -- remote execution using docker containers")
 
-cmd = "docker build . -t {0}".format(DEfAULT_IMAGE)
-print (cmd)
-os.system(cmd)
+def rexec(url, args):
+    print ("REXEC", url)
 
-args = " ".join(sys.argv[1:])
-path = os.path.abspath('.')
-cmd = "docker run --rm -it -v {2}:/home/rexec {0} {1}".format(DEfAULT_IMAGE, args, path)
-print (cmd)
-os.system(cmd)
+    remote = ""
+    if url:
+        remote = "-H " + url
+    cmd = "docker {1} build . -t {0}".format(DEfAULT_IMAGE, remote)
+    print (cmd)
+    os.system(cmd)
+
+    args = " ".join(args)
+    path = os.path.abspath('.')
+    cmd = "docker {3} run --rm -it -v {2}:/home/rexec {0} {1}".format(DEfAULT_IMAGE, args, path, remote)
+    print (cmd)
+    os.system(cmd)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--url")
+    args, unknown = parser.parse_known_args()
+    rexec(args.url, unknown)
