ae2131f6175fa5f2c0eebc1f4b073b9f9dba79e0
danx0r <dan.miller@eye0.com>
2020-07-16 14:26:15 -0700

    libcloud FTW

--- rexec/lcloud.py
+++ rexec/lcloud.py
@@ -4,4 +4,12 @@ from libcloud.compute.providers import get_driver
 import config
 
 cls = get_driver(Provider.EC2)
-driver = cls(config.access, config.secret)
+driver = cls(config.access, config.secret, region=config.region)
+# pprint(driver.list_sizes())
+# pprint(driver.list_nodes())
+
+nodes = driver.list_nodes()
+node = [x for x in nodes if '44.231.174.47' in x.public_ips][0]
+pprint(node)
+# driver.start_node(node)
+driver.stop_node(node)
