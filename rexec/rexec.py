26b02527609f90ae5e1f408cadd63eb6ee05a8a1
danx0r <dan.miller@eye0.com>
2020-07-23 14:04:54 -0700

    always use protection when available

--- rexec/rexec.py
+++ rexec/rexec.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os, sys, argparse, subprocess, time
+import os, sys, argparse, subprocess, time, traceback
 from lcloud import *
 from runrun import run
 DEFAULT_IMAGE = "rexec_image"
@@ -7,73 +7,86 @@ DOCKER_REMPORT = "2376"
 DOCKER_REMOTE = "localhost:"+DOCKER_REMPORT
 
 def rexec(args, user=None, url=None, uuid=None, name=None, gpus = "", ports=None, stop=False):
-    if url:
-        if not user:
-            user, url = url.split('@')
-    node = None
-    if url or uuid or name:
-        node = get_server(url=url, uuid=uuid, name=name)
-        if node:
-            url = node.public_ips[0]
-            if node.state.lower() != "running":
-                print ("Starting", url)
-                start_server(node)
-                print ("Waiting for sshd")
-                cmd = ["ssh", "{0}@{1}".format(user, url), "echo", "'sshd responding'"]
-                print(cmd)
-                ret = run(cmd)
-                if ret[0].strip()!='sshd responding':
-                    print ("error in ssh call:", ret[0].strip())
-                    return
-                print ("SSH returns -->%s|%s<--" % ret)
+    tunnel = None
+    try:
+        if url:
+            if not user:
+                user, url = url.split('@')
+        node = None
+        if url or uuid or name:
+            node = get_server(url=url, uuid=uuid, name=name)
+            if node:
+                url = node.public_ips[0]
+                if node.state.lower() != "running":
+                    print ("Starting", url)
+                    start_server(node)
+                    print ("Waiting for sshd")
+                    cmd = ["ssh", "{0}@{1}".format(user, url), "echo", "'sshd responding'"]
+                    print(cmd)
+                    good = False
+                    for z in range(3, -1, -1):
+                        ret = run(cmd)
+                        if ret[0].strip()=='sshd responding':
+                            good = True
+                            break
+                        print ("sshd not responding; %d attempts left" % z)
+                        if z:
+                            time.sleep(10)
+                    if not good:
+                        raise Exception("error in ssh call: %s" % ret[0].strip())
+                    print ("SSH returns -->%s|%s<--" % ret)
+            else:
+                raise Exception("Error: node not found")
+
+        if url:
+            print ("rexec: running on", url)
+            ssh_args = ["ssh", "-NL", "{0}:/var/run/docker.sock".format(DOCKER_REMPORT), "{0}@{1}".format(user, url)]
+            print (ssh_args)
+            tunnel = subprocess.Popen(ssh_args)
+            time.sleep(5)                                    #FIXME get smarter -- wait for output confirming tunnel is live
+            remote = "-H " + DOCKER_REMOTE
+            relpath = os.path.abspath('.')[len(os.path.expanduser('~')):]
+            relpath = "/_REXEC" +  relpath.replace('/', '_') #I can exlain
+            locpath = os.path.abspath('.')
+            path = "/home/{0}{1}".format(user, relpath)
+            cmd = "rsync -vrltzu {0}/* {3}@{1}:{2}/".format(locpath, url, path, user)
+            print (cmd)
+            os.system(cmd)
         else:
-            print ("Error: node not found")
-            return
+            print ("rexec: running locally")
+            remote = ""
+            path = os.path.abspath('.')
 
-    if url:
-        print ("rexec: running on", url)
-        ssh_args = ["ssh", "-NL", "{0}:/var/run/docker.sock".format(DOCKER_REMPORT), "{0}@{1}".format(user, url)]
-        print (ssh_args)
-        # return
-        tunnel = subprocess.Popen(ssh_args)
-        time.sleep(5)                                    #FIXME get smarter -- wait for output confirming tunnel is live
-        remote = "-H " + DOCKER_REMOTE
-        relpath = os.path.abspath('.')[len(os.path.expanduser('~')):]
-        relpath = "/_REXEC" +  relpath.replace('/', '_') #I can exlain
-        locpath = os.path.abspath('.')
-        path = "/home/{0}{1}".format(user, relpath)
-        cmd = "rsync -vrltzu {0}/* {3}@{1}:{2}/".format(locpath, url, path, user)
+        cmd = "docker {1} build . -t {0}".format(DEFAULT_IMAGE, remote)
         print (cmd)
         os.system(cmd)
-    else:
-        print ("rexec: running locally")
-        remote = ""
-        path = os.path.abspath('.')
 
-    cmd = "docker {1} build . -t {0}".format(DEFAULT_IMAGE, remote)
-    print (cmd)
-    os.system(cmd)
-
-    args = " ".join(args)
-    gpu_args = "--gpus "+gpus if gpus else ""
-    port_args = ""
-    if ports:
-        for pa in ports:
-            if ':' not in pa:
-                pa = "{0}:{0}".format(pa)
-            port_args += " -p " + pa
-    cmd = "docker {3} run {4} {5} --rm -it -v {2}:/home/rexec {0} {1}".format(DEFAULT_IMAGE,
-                                                                              args, path, remote, gpu_args, port_args)
-    print (cmd)
-    os.system(cmd)
-    if url:
-        cmd = "rsync -vrltzu '{3}@{1}:{2}/*' {0}/".format(locpath, url, path, user)
+        args = " ".join(args)
+        gpu_args = "--gpus "+gpus if gpus else ""
+        port_args = ""
+        if ports:
+            for pa in ports:
+                if ':' not in pa:
+                    pa = "{0}:{0}".format(pa)
+                port_args += " -p " + pa
+        cmd = "docker {3} run {4} {5} --rm -it -v {2}:/home/rexec {0} {1}".format(DEFAULT_IMAGE,
+                                                                                  args, path, remote, gpu_args, port_args)
         print (cmd)
         os.system(cmd)
+        if url:
+            cmd = "rsync -vrltzu '{3}@{1}:{2}/*' {0}/".format(locpath, url, path, user)
+            print (cmd)
+            os.system(cmd)
+    except:
+        traceback.print_exc()
+        print ("--------------------------------")
+
+    if url:
         if stop and node:
             print ("Stopping VM at", url)
             stop_server(node)
-        tunnel.kill()
+        if tunnel:
+            tunnel.kill()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
