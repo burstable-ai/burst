ba94b3e005d51b3b3b517e28dbbd28282ca32e0d
danx0r <dan.miller@eye0.com>
2020-07-28 13:06:02 -0700

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="rexec",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     python_requires='>=3',
     scripts=['bin/rexec'],
