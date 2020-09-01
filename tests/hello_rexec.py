3242e41c32e3685c3984e1d20de14ec616550958
danx0r <dan.miller@eye0.com>
2020-09-01 13:41:27 -0700

    test for GPU's

--- tests/hello_rexec.py
+++ tests/hello_rexec.py
@@ -1,5 +1,13 @@
-import multiprocessing
+import multiprocessing, GPUtil
 
 
-print ("Welcome rexec-utioners! You're running virtual machine with %i cpus" % (multiprocessing.cpu_count()))
+print ("Welcome rexec-utioners! You're running a virtual machine with %i cpus" % (multiprocessing.cpu_count()))
 
+try:
+    avail = GPUtil.getAvailable()
+    if len(avail):
+        print ("The following GPU's are available: %s" % avail)
+    else:
+        print ("GPU drivers are installed but no GPU's are available")
+except:
+    print ("No GPU drivers available")
