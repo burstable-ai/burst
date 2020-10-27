7e413640c1d222794c354dea51512e412bee372b
danx0r <dan.miller@eye0.com>
2020-10-27 13:53:00 -0700

    datahubdock --> burstableai

--- burst/burst.py
+++ burst/burst.py
@@ -20,7 +20,7 @@ from burst.verbos import set_verbosity, get_verbosity, vprint, vvprint, v0print,
 os.chdir(opath)
 
 DEFAULT_IMAGE = "burst_image" #FIXME: should be unique to folder structure
-SHUTDOWN_IMAGE = "datahubdock/burst:burst_shutdown"
+SHUTDOWN_IMAGE = "burstableai/burst:burst_shutdown"
 DOCKER_REMPORT = "2376"
 DOCKER_REMOTE = "localhost:"+DOCKER_REMPORT
 
