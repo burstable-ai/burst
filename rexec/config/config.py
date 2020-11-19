79bc4d1cdaceab29490cc30c52753376424fa51e
Chris LeBailly <chris.lebailly@gmail.com>
2020-11-19 12:19:18 -0800

    Updated welcome message

--- rexec/config/config.py
+++ rexec/config/config.py
@@ -12,7 +12,7 @@ def main():
     args = parse_arguments()
 
     config = get_config(CONFIG_FILE)
-    print('Welcome to rexec!')
+    print('Welcome to rexec configuration!')
     if args.main_choice in [None, 'summary']:
         summary.all(config)
 
