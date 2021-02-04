ce1f0cc87814dc505c66725eacef350e3b64839c
danx0r <dan.miller@eye0.com>
2021-02-04 15:41:39 -0800

    quicktest

--- tests/quicktest.py
+++ tests/quicktest.py
@@ -1,15 +1,14 @@
 import os, sys
 
 out1 = """---------------------OUTPUT-----------------------
-Build phase 1 success
-----------------------END-------------------------"""
+Welcome burstables! You're running a virtual machine with"""
 
-out2 = """Build phase 2 success"""
+out2 = "----------------------END-------------------------"
 
-os.system("rm buildtest.log")
-os.system("burst --build 2>&1 | tee buildtest.log")
+os.system("rm quicktest.log")
+os.system("burst --verbosity 1 'python3 hello_burst.py' 2>&1 | tee quicktest.log")
 
-f = open("buildtest.log")
+f = open("quicktest.log")
 s = f.read()
 f.close()
 
