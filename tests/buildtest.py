fc711ca2e135a7057e47f1a624ade826211c5554
danx0r <dan.miller@eye0.com>
2021-02-05 14:31:15 -0800

    relax don't do it...

--- tests/buildtest.py
+++ tests/buildtest.py
@@ -1,13 +1,11 @@
 import os, sys
 
-out1 = """---------------------OUTPUT-----------------------
-Build phase 1 success
-----------------------END-------------------------"""
+out1 = """Build phase 1 success"""
 
 out2 = """Build phase 2 success"""
 
 os.system("rm buildtest.log")
-os.system("burst --build 2>&1 | tee buildtest.log")
+os.system("burst --verbosity 127 --build 2>&1 | tee buildtest.log")
 
 f = open("buildtest.log")
 s = f.read()
--- tests/quicktest.py
+++ tests/quicktest.py
@@ -1,12 +1,11 @@
 import os, sys
 
-out1 = """---------------------OUTPUT-----------------------
-Welcome burstables! You're running a virtual machine with"""
+out1 = """Welcome burstables! You're running a virtual machine with"""
 
 out2 = "----------------------END-------------------------"
 
 os.system("rm quicktest.log")
-os.system("burst --verbosity 1 'python3 hello_burst.py' 2>&1 | tee quicktest.log")
+os.system("burst --verbosity 127 'python3 hello_burst.py' 2>&1 | tee quicktest.log")
 
 f = open("quicktest.log")
 s = f.read()
@@ -17,3 +16,4 @@ if out1 in s and out2 in s:
     print ("PASSED")
 else:
     print ("FAILED")
+
