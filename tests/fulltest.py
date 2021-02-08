d4ccc501220a1e453144d85e10a80ba405e04f20
danx0r <dan.miller@eye0.com>
2021-02-08 14:12:59 -0800

    fulltest coming along nicely ty

--- tests/fulltest.py
+++ tests/fulltest.py
@@ -1,11 +1,28 @@
-import os, sys
+import os, sys, argparse
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--cloudmap", required=True, help="must specify cloudmap; a test file will be written here")
+parser.add_argument("--storage-config")
+parser.add_argument("--compute-config")
+parser.add_argument("--verbosity", type=int, default=1)
+args = parser.parse_args()
 
 out1 = """Welcome burstables! You're running a virtual machine with"""
 
 out2 = "----------------------END-------------------------"
 
 os.system("rm fulltest.log")
-os.system("burst --verbosity 127 --cloudmap s3:s3_data 'python3 fulltest_command.py' 2>&1 | tee fulltest.log")
+opts = "--cloudmap={0} --verbosity={1}".format(args.cloudmap, args.verbosity)
+if args.storage_config:
+    opts += " --storage-config={0}".format(args.storage_config)
+if args.compute_config:
+    opts += " --compute-config={0}".format(args.compute_config)
+
+cpath = args.cloudmap.split(':')[1]
+
+cmd = "burst {0} 'python3 fulltest_command.py --cloudpath {1}' 2>&1 | tee fulltest.log".format(opts, cpath)
+print (cmd)
+os.system(cmd)
 
 f = open("fulltest.log")
 s = f.read()
--- tests/fulltest_command.py
+++ tests/fulltest_command.py
@@ -1,5 +1,13 @@
-import os, sys
+import os, sys, argparse
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--cloudpath", required=True)
+args = parser.parse_args()
 
 print ("fulltest_command:")
-os.system("ls s3_data")
+
+cmd = "ls {0}".format(args.cloudpath)
+print (cmd)
+os.system(cmd)
+
 sys.stdout.flush()
