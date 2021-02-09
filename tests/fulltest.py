1355a7aa13532acf17df6d2b252f802ee39cab7b
danx0r <dan.miller@eye0.com>
2021-02-08 16:07:35 -0800

    fulltest coming along nicely still

--- tests/fulltest.py
+++ tests/fulltest.py
@@ -1,26 +1,27 @@
 import os, sys, argparse
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("--cloudmap", required=True, help="must specify cloudmap; a test file will be written here")
+parser.add_argument("--cloudmap", required=True, help="as passed to burst")
+parser.add_argument("--testpath", required=True, help="bucket or root directory for tests")
 parser.add_argument("--storage-config")
 parser.add_argument("--compute-config")
 parser.add_argument("--verbosity", type=int, default=1)
 args = parser.parse_args()
 
-out1 = """Welcome burstables! You're running a virtual machine with"""
+out1 = "----------------------END-------------------------"
 
-out2 = "----------------------END-------------------------"
+out2 = "123\n456\n"
 
-os.system("rm fulltest.log")
-opts = "--cloudmap={0} --verbosity={1}".format(args.cloudmap, args.verbosity)
+os.system("rm fulltest.log fulltest.foo")
+opts = "--verbosity={0} --cloudmap={1}".format(args.verbosity, args.cloudmap)
 if args.storage_config:
     opts += " --storage-config={0}".format(args.storage_config)
 if args.compute_config:
     opts += " --compute-config={0}".format(args.compute_config)
 
-cpath = args.cloudmap.split(':')[1]
+root = args.cloudmap.split(':')[1]
 
-cmd = "burst {0} 'python3 fulltest_command.py --cloudpath {1}' 2>&1 | tee fulltest.log".format(opts, cpath)
+cmd = "burst {0} 'python3 fulltest_command.py --testpath={1}/{2}' 2>&1 | tee fulltest.log".format(opts, root, args.testpath)
 print (cmd)
 os.system(cmd)
 
@@ -28,8 +29,15 @@ f = open("fulltest.log")
 s = f.read()
 f.close()
 
+foo = None
+if os.path.exists("fulltest.foo"):
+    f = open("fulltest.foo")
+    foo = f.read()
+    f.close()
+
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TEST COMPLETED~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-if out1 in s and out2 in s:
+
+if out1 in s and out2 == foo:
     print ("PASSED")
 else:
     print ("FAILED")
--- tests/fulltest_command.py
+++ tests/fulltest_command.py
@@ -1,13 +1,19 @@
 import os, sys, argparse
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("--cloudpath", required=True)
+parser.add_argument("--testpath", required=True)
 args = parser.parse_args()
+
+def do(cmd):
+    print (cmd)
+    os.system(cmd)
 
 print ("fulltest_command:")
 
-cmd = "ls {0}".format(args.cloudpath)
-print (cmd)
-os.system(cmd)
+do("rm fulltest.foo")
+do("echo 123 > {0}/foo".format(args.testpath))
+do("echo 456 >> {0}/foo".format(args.testpath))
+do("cp {0}/foo fulltest.foo".format(args.testpath))
+do("rm {0}/foo".format(args.testpath))
 
 sys.stdout.flush()
