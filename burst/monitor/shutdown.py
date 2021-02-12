e86e95b2acd8e33cbbb5174838cfa6595b46a207
danx0r <dan.miller@eye0.com>
2021-02-11 19:02:58 -0800

    basics

--- burst/monitor/shutdown.py
+++ burst/monitor/shutdown.py
@@ -2,13 +2,19 @@ import os, sys, json, time, subprocess
 from pprint import pprint
 
 while True:
-    proc = subprocess.Popen(["docker",  "ps", "--format='{{json .}}'"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    out = proc.stdout.read()
-    if out:
-        out = eval("""r%s""" % out.decode())
-        print(out)
+    proc = subprocess.Popen(["docker", "ps", "--format='{{json .}}'"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    count  = 0
+    lines = proc.stdout.read().strip().split(b"\n")
+    for out in lines:
+        out = out.decode().strip()[1:-1] #seems a docker bug; returning single-quoted json blob
+        # print("OUT:", out)
+        if not out:
+            continue
         j = json.loads(out)
-        pprint(j)
-    else:
+        # pprint(j)
+        print ("RUNNING:", j['Image'], j['Labels'])
+        count += 1
+        # if j[]
+    if count==0:
         print ("NADA")
-    time.sleep(7)
+    time.sleep(5)
