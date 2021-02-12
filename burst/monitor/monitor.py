0868369d85ff061773ca0fc70b18fa6adffee4fd
danx0r <dan.miller@eye0.com>
2021-02-12 13:19:39 -0800

    shutdown logic

--- burst/monitor/monitor.py
+++ burst/monitor/monitor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-import os, sys, json, time, subprocess
+import os, sys, json, time, subprocess, datetime
 from pprint import pprint
+
+shuttime = None
 
 while True:
     proc = subprocess.Popen(["docker", "ps", "--format='{{json .}}'"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -14,8 +16,25 @@ while True:
         j = json.loads(out)
         # pprint(j)
         print ("RUNNING:", j['Image'], j['Labels'])
+        for x in j['Labels'].split(','):
+            if 'burstable' in x:
+                key, val = x.split('=')
+                print ("LABEL: %s = %s" % (key, val))
+                if key == 'ai.burstable.shutdown':
+                    delay = int(val)
+                    t = datetime.datetime.utcnow() + datetime.timedelta(seconds = delay)
+                    if shuttime == None or shuttime < t:
+                        shuttime = t
+                else:
+                    print ("ERROR -- unknown docker label %s=%s" % (key, val))
         count += 1
-        # if j[]
     if count==0:
         print ("NADA")
+
+    now = datetime.datetime.utcnow()
+    remain = (shuttime-now) if shuttime else None
+    print ("time now:", now, "shutoff time:", shuttime, "remaining:", remain.total_seconds() if remain else None )
+    if remain != None and remain < datetime.timedelta(seconds=0):
+        print ("Proceeding to shutdown")
+
     time.sleep(5)
