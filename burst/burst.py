e178d96d31e63512c63d2addd8417c38b142b3de
danx0r <dan.miller@eye0.com>
2021-02-22 14:15:15 -0800

    Sentinel for rsync hax

--- burst/burst.py
+++ burst/burst.py
@@ -22,6 +22,14 @@ os.chdir(opath)
 DEFAULT_IMAGE = "burst_image" #FIXME: should be unique to folder structure
 MONITOR_IMAGE = "burstableai/burst_monitor:latest"
 
+sentinel_py = """
+import os, time, datetime
+while True:
+    os.system("echo %s > burst-monitor.txt" % datetime.datetime.utcnow())
+    os.system("ps ax >> burst-monitor.txt")
+    time.sleep(5)
+"""
+
 def burst(args, sshuser=None, url=None, uuid=None, burst_user=None, gpus = "", ports=None, stop=False,
           image=None, size=None, pubkey=None, dockerfile="Dockerfile",
           cloudmap="", dockerdport=2376, conf=None):
@@ -153,29 +161,17 @@ and files that are referred to (such as requirements.txt) to the build daemon.
                             monitor_running = True
             vprint ("monitor_running: %s" % monitor_running)
 
-            # #if any docker shutdown processes are running, kill
-            # if out[0].strip():
-            #
-            #     #kill any pending shutdown containers FIXME: should only fix shutdowns associated with session
-            #     kills = []
-            #     for x in out[0].split("\n"):
-            #         if x:
-            #             try:
-            #                 j = json.loads(x)
-            #             except:
-            #                 raise Exception("ERROR in Docker check (is Docker installed?): %s" % x)
-            #             Command = j['Command']
-            #             if Command.find("burst --stop") < 2:
-            #                 kills.append(j['ID'])
-            #     if kills:
-            #         vprint ("Killing shutdown processes:", kills)
-            #         cmd = "docker {0} stop {1} {2} &".format(remote, " ".join(kills), get_piper())
-            #         vvprint (cmd)
-            #         os.system(cmd)
-
             #if restarted (including fresh launch), start monitor docker process
             if restart or not monitor_running:
                 vprint ("Starting monitor process for shutdown++")
+
+                #set up sentinel script in detached screen on host (not docker) to help check on rsync
+                cmd = 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=error {0}@{1} ' \
+                      '"screen -md -S screentest"'.format(sshuser, url)
+                vvprint(cmd)
+                os.system(cmd)
+
+                #run monitor (in docker container) to check if user's burst OR rsync is still running
                 conf = get_config()
                 if conf.provider == "GCE":
                     secret = ".burst/" + conf.raw_secret
