35edbc7274a2ca9debeac0f1cf0dd5e33eebdbbc
danx0r <dan.miller@eye0.com>
2021-03-11 17:02:35 -0800

    background testing

---
+++ tests/long_process.py
@@ -0,0 +1,19 @@
+import sys, time, os
+t0 = time.time()
+print ("HOME2\nPublic ip:", end=' ')
+sys.stdout.flush()
+os.system("curl https://ipv4.wtfismyip.com/text")
+print ("This is a long-running process (of sorts)")
+print (os.path.abspath('.'))
+f = open("./data/test.log", 'w')
+for i in range(int(sys.argv[1])):
+    t = time.time()-t0
+    print (i, "TIME:", t)
+    sys.stdout.flush()
+    print (i, "TIME:", t, file=f)
+    f.flush()
+    time.sleep(5)
+f.close()
+
+print ("OK, done with that. I should be going to sleep now...")
+sys.stdout.flush()
